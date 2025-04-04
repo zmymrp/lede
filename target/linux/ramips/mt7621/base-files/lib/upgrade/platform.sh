1aac882a138995e784991242e24797ad32f0e1cd
zmymrp <zmymrp@qq.com>
2025-04-05 00:11:57 +0800

    Update platform.sh

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -78,6 +78,7 @@ platform_do_upgrade() {
 	beeline,smartbox-giga|\
 	beeline,smartbox-turbo-plus|\
 	c-life,xg1|\
+	cmcc,g-ax1800-f|\
 	dlink,dir-1960-a1|\
 	dlink,dir-2640-a1|\
 	dlink,dir-2660-a1|\
